8d486bae53585ed71bdefb981177ad97e2bb9581
William B <william@barkoffusa.com>
2017-09-16 13:31:03 -0400

    Add Atlas Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Atlas.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Atlas.java
@@ -107,33 +107,41 @@ public class Atlas extends OpMode
     @Override
     public void loop() {
         // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+        if (gamepad1.right_stick_x != 0) {
+            FrontLeftDrive.setPower(gamepad1.right_stick_x);
+            BackLeftDrive.setPower(-gamepad1.right_stick_x);
+            FrontRightDrive.setPower(-gamepad1.right_stick_x);
+            BackRightDrive.setPower(gamepad1.right_stick_x);
+        } else if (gamepad1.right_stick_y != 0) {
+            FrontLeftDrive.setPower(gamepad1.right_stick_x);
+            BackLeftDrive.setPower(-gamepad1.right_stick_x);
+            FrontRightDrive.setPower(-gamepad1.right_stick_x);
+            BackRightDrive.setPower(gamepad1.right_stick_x);
+        } else {
+            double leftPower;
+            double rightPower;
 
-        // Choose to drive using either Tank Mode, or POV Mode
-        // Comment out the method that's not used.  The default below is POV.
+            // Choose to drive using either Tank Mode, or POV Mode
+            // Comment out the method that's not used.  The default below is POV
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            double drive = -gamepad1.left_stick_y;
+            double turn  =  gamepad1.left_stick_x;
+            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
-        // POV Mode uses left stick to go forward, and right stick to turn.
-        // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+            // Tank Mode uses one stick to control each wheel.
+            // - This requires no math, but it is hard to drive forward slowly and keep straight.
+            // leftPower  = -gamepad1.left_stick_y ;
+            // rightPower = -gamepad1.right_stick_y ;
 
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
-        // leftPower  = -gamepad1.left_stick_y ;
-        // rightPower = -gamepad1.right_stick_y ;
-
-        // Send calculated power to wheels
-        FrontLeftDrive.setPower(leftPower);
-        FrontRightDrive.setPower(rightPower);
-        BackLeftDrive.setPower(leftPower);
-        BackRightDrive.setPower(rightPower);
-
-        // Show the elapsed game time and wheel power.
+            // Send calculated power to wheels
+            FrontLeftDrive.setPower(leftPower);
+            FrontRightDrive.setPower(rightPower);
+            BackLeftDrive.setPower(leftPower);
+            BackRightDrive.setPower(rightPower);
+        }
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
     }
 
     /*
