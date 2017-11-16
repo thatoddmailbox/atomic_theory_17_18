cc64b3cb8f80cc16092b36c8855fd6aca0b9ca0e
Ava Crnkovic-Rubsamen <student@avacrnkrubsamen.dalton.org>
2017-11-15 20:32:26 -0500

    gets jewel consistently & glyph inconsistently

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousOperation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousOperation.java
@@ -24,11 +24,22 @@ public abstract class AutonomousOperation extends LinearOpMode {
         while (opModeIsActive()) {
             robot.jewelArmLower.setPosition(0.3);
             robot.LiftMotor.setPower(0.5);
-            sleep(300);
+            sleep(400);
             robot.LiftMotor.setPower(0);
             robot.LeftArmServo.setPosition(0.6);
-            robot.RightArmServo.setPosition(0.6);
+            robot.RightArmServo.setPosition(0.4);
             sleep(700);
+            robot.LiftMotor.setPower(-0.5);
+            sleep(250);
+            robot.LiftMotor.setPower(0);
+            sleep(500);
+            robot.LeftArmServo.setPosition(0.75);
+            robot.RightArmServo.setPosition(0.35);
+            sleep(700);
+            robot.LiftMotor.setPower(0.5);
+            sleep(700);
+            robot.LiftMotor.setPower(0);
+
 
             Alliance rightBallColor = (robot.jewelColor.blue() > robot.jewelColor.red() ? Alliance.BLUE : Alliance.RED);
             boolean shouldHitRight = (rightBallColor == Alliance.RED && getAlliance() == Alliance.RED) || (rightBallColor == Alliance.BLUE && getAlliance() == Alliance.BLUE);
@@ -38,12 +49,16 @@ public abstract class AutonomousOperation extends LinearOpMode {
                 telemetry.update();
                 robot.straightDrive(-0.4, 0.4);
                 sleep(300);
+                robot.straightDrive(0,0);
+                sleep(300);
                 robot.jewelArmLower.setPosition(0.7);
                 robot.straightDrive(0.3, -0.3);
             } else {
                 telemetry.addData("hitting", "left");
                 telemetry.update();
                 robot.straightDrive(0.4, -0.4);
+                sleep(300);
+                robot.straightDrive(0,0);
                 sleep(300);
                 robot.jewelArmLower.setPosition(0.7);
                 robot.straightDrive(-0.3, 0.3);
@@ -55,12 +70,26 @@ public abstract class AutonomousOperation extends LinearOpMode {
 
 
             robot.strafeLeft(0.7);
-            sleep(2800);
+            sleep(2400);
+            robot.straightDrive(1, 1);
+            sleep(75);
+            robot.straightDrive(0,0);
+            sleep(200);
             robot.straightDrive(-1, 1);
             sleep(1000);
+            robot.straightDrive(0,0);
+            sleep(200);
             robot.straightDrive(0.8, 0.8);
-            sleep(300);
-
+            sleep(350);
+            robot.straightDrive(0,0);
+            sleep(200);
+            robot.LeftArmServo.setPosition(0.6);
+            robot.RightArmServo.setPosition(0.4);
+            sleep(700);
+            robot.straightDrive(-0.4, -0.4);
+            sleep(100);
+            robot.straightDrive(0,0);
+            sleep(200);
             idle();
             break;
         }
